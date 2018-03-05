36eb23cf273d1f7e6993a2036436b4fcbc4df785
vanshergzie <jaywizzy34@gmail.com>
2018-03-05 18:47:19 +0100

    first

--- calendarapp/settings.py
+++ calendarapp/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'calendarapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'staticfiles')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
