0e8dc9075537da2f80c100d226f356eca58a141c
vanshergzie <jaywizzy34@gmail.com>
2018-03-05 14:36:26 +0100

    changed the python version to 3.6

--- calendarapp/settings.py
+++ calendarapp/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mprnl@6al88sjcr=1cnu5a6lmx6#%!qu&dch+3#lquyj(aj#5c'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
